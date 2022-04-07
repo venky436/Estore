ccd47a6035d1068d53b7c35274479794038cdd67
venkatesh <venkateshpannuri436@gmail.com>
2022-04-07 15:59:28 +0530

    add

--- src/Actions/CartAction.js
+++
@@ -1,9 +0,0 @@
-// import axios from 'axios'
-
-export const cartAction = (id) =>(getState,dispatch)=>{
-    dispatch({
-        type : 'ADD_TO_CART',
-        payload : id
-    })
-    localStorage.setItem('cartItems',JSON.stringify(getState().cart.cartItems))
-}
