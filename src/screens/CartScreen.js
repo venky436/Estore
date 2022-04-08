e4b19fb01ebb67daf9941ebebd4aaeb2fbaf267f
venkatesh <venkateshpannuri436@gmail.com>
2022-04-08 09:01:26 +0530

    add

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -7,6 +7,7 @@ import '../Css/CartScreen.css'
 
 import { Store } from '../App'
 import { cartItem } from './ProductScreen'
+import { Link } from 'react-router-dom'
 
 
 export let cart_count = 0
@@ -71,72 +72,77 @@ let price = cartItem && cartItem.reduce((acc,ele)=>acc+(ele.price*ele.qty),0)
     }, [items]);
   return (
     <div>
-      <div className="container" style={{ marginTop: "19rem" }}>
-        <div className="row">
-          <div className="col-md-8">
-            <table className="table d-block w-100 h-100">
-              {items ? (
-                <thead>
-                  <tr>
-                    <td>&nbsp;</td>
-                    <td>Name</td>
-                    <td>Quntity</td>
-                    <td>Price</td>
-                    <td>Total</td>
-                    <td>Edit</td>
-                  </tr>
-                </thead>
-              ) : (
-                <h1>Your Cart Is Empty</h1>
-              )}
-
-              <tbody>
-                {items
-                  ? items.map((ele, index) => (
-                      <tr key={index}>
-                        <td>
-                          <img src={ele.image} id="image" />
-                        </td>
-                        <td>{ ele.title}</td>
-                        <td>{ ele.qty}</td>
-                        <td>${ele.price}</td>
-                        <td>${(ele.price * ele.qty).toFixed(2)}</td>
-                        <td>
-                          <button id="btn" onClick={() => removeHandler(ele)}>
+      {items.length > 0 ? (
+        <div className="container" style={{ marginTop: "19rem" }}>
+          <div className="row">
+            <div className="col-md-8">
+              {items &&
+                items.map((e, index) => (
+                  <ListGroup key={index}>
+                    <ListGroup.Item>
+                      <div className="row">
+                        <div className="col-md-1">
+                          <img src={e.image} className="d-block w-100" />
+                        </div>
+                        <div className="col-md-3">
+                          <h3>{e.title}</h3>
+                        </div>
+                        <div className="col-md-2">
+                          <h3>{e.qty}</h3>
+                        </div>
+                        <div className="col-md-2">
+                          <h3>${e.price}</h3>
+                        </div>
+                        <div className="col-md-2">
+                          <h3>${e.qty * e.price}</h3>
+                        </div>
+                        <div className="col-md-2">
+                          <button id="btn" onClick={() => removeHandler(e)}>
                             <i className="fas fa-trash" title="Remove"></i>
                           </button>
-                        </td>
-                      </tr>
-                    ))
-                  : ""}
-              </tbody>
-            </table>
-          </div>
-          <div className="col-3 col-md-3 col-lg-3 py-3">
-            <div className="card">
-              <div className="card-body">
-                <h3 className="card-title">
-                  <span>Total Items</span> : <span>({count})</span>
-                </h3>
-                <hr />
-                <h3 className="card-title">
-                  <span>Total Price</span> :{" "}
-                  <span>$ {price ? price.toFixed(2) : 0}</span>
-                </h3>
-                <hr />
-                <h3 className="card-title">
-                  <span>Shipping Price</span> :{" "}
-                  <span>$ {price > 100 ? 14 : 0}</span>
-                </h3>
-                <hr />
-                <button className="btn btn-primary d-block w-100 fs-3" disabled={items.length > 0 ? false : true} onClick={placeHandler}>
-                  Place Order
-                </button>
+                        </div>
+                      </div>
+                    </ListGroup.Item>
+                  </ListGroup>
+                ))}
+            </div>
+            <div className="col-3 col-md-3 col-lg-3 py-3">
+              <div className="card">
+                <div className="card-body">
+                  <h3 className="card-title">
+                    <span>Total Items</span> : <span>({count})</span>
+                  </h3>
+                  <hr />
+                  <h3 className="card-title">
+                    <span>Total Price</span> :{" "}
+                    <span>$ {price ? price.toFixed(2) : 0}</span>
+                  </h3>
+                  <hr />
+                  <h3 className="card-title">
+                    <span>Shipping Price</span> :{" "}
+                    <span>$ {price > 100 ? 14 : 0}</span>
+                  </h3>
+                  <hr />
+                  <button
+                    className="btn btn-primary d-block w-100 fs-3"
+                    disabled={items.length > 0 ? false : true}
+                    onClick={placeHandler}
+                  >
+                    Place Order
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <h1 id="empty">
+          Your cart Is Empty{" "}
+          <Link to="/">
+            <span id='shop'>Shop Now</span>
+          </Link>
+        </h1>
+      )}
     </div>
   );
 }
